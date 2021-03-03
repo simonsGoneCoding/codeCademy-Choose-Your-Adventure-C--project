227f64506059725829ce30c11f958c0baafabf89
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-03 16:21:40 +0000

    Complete all bulletpoints.

--- ChooseYourAdventure/ChooseYourAdventure/Program.cs
+++ ChooseYourAdventure/ChooseYourAdventure/Program.cs
@@ -13,7 +13,61 @@ namespace ChooseYourAdventure
             string name = Console.ReadLine();
             Console.WriteLine($"Hello, {name}! Welcome to our story.");
 
-            
+            Console.WriteLine("It begins on a cold rainy night. You're sitting in your room and hear a noise coming from down the hall. Do you go investigate?");
+            Console.Write("Type YES or NO:  ");
+            string noiseChoice = Console.ReadLine();
+            noiseChoice = noiseChoice.ToUpper();
+
+
+            // Adding Conditional Statements
+            if(noiseChoice == "NO")
+            {
+                Console.WriteLine("Not much of an adventure if we don't leave our room!");
+                Console.WriteLine("THE END.");
+            }
+            else if(noiseChoice == "YES")
+            {
+                Console.WriteLine("You walk into the hallway and see a light coming from under a door down the hall. You walk towards it.\nDo you open it or knock ? ");
+                Console.Write("Type OPEN or KNOCK:   "); 
+                string doorChoice = Console.ReadLine().ToUpper();
+                
+                if(doorChoice == "KNOCK")
+                {
+                    Console.WriteLine("A voice behind the door speaks. It says, \"Answer this riddle:\"\n\"Poor people have it. Rich people need it. If you eat it you die. What is it?\"");
+                    Console.Write("Type your answer: "); 
+                    string riddleAnswer = Console.ReadLine().ToUpper();
+
+                    if(riddleAnswer == "NOTHING")
+                    {
+                        Console.WriteLine("The door opens and NOTHING is there.\nYou turn off the light and run back to your room and lock the door.\n THE END.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You answered incorrectly. The door doesn't open.\nTHE END.");
+                    }
+                }
+                else if (doorChoice == "OPEN")
+                {
+                    Console.WriteLine("The door is locked! See if one of your three keys will open it.\n Enter a number 1-3:");
+                    string keyChoice = Console.ReadLine().ToUpper();
+
+                    switch (keyChoice)
+                    {
+                        case "1":
+                            Console.WriteLine("You choose the first key. Lucky choice!\nThe door opens and NOTHING is there. Strange...\nTHE END.");
+                            break;
+
+                        case "2":
+                            Console.WriteLine("You choose the second key. The door doesn't open.\nTHE END.");
+                            break;
+
+                        case "3":
+                            Console.WriteLine("You choose the second key. The door doesn't open.\nTHE END.");
+                            break;
+                    }
+                    
+                }
+            }   
         }
     }
 }
