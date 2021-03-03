00eeef2db2fd1a4486ffbec1baf672591196882f
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-03 16:18:22 +0000

    Initial commit - codeCademy - logic and conditionals C# project.

---
+++ ChooseYourAdventure/ChooseYourAdventure/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChooseYourAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* THE MYSTERIOUS NOISE */
+
+            // Start by asking for the user's name:
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Welcome to our story.");
+
+            
+        }
+    }
+}
